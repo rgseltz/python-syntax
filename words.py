aea77b5ea8f1a1e92b94c77def14fe7ab87029a4
ryan <ryanjgecelter@gmail.com>
2022-03-06 14:15:00 +0200

    Completed exercise files minus errors in converter file

---
+++ words.py
@@ -0,0 +1,8 @@
+def print_upper_words(str):
+    """ Return a string as uppercase letters"""
+    if str[0] == "e":
+        return print(str.upper())
+    print("If the first word is not an e, no uppercase for you")
+    return print(str)
+    
+
